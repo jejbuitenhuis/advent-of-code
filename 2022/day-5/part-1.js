eb93c45a7e3e1db96c2ef39dfd79c2fbd33e1485
jejbuitenhuis <jorambuitenhuis@gmail.com>
2022-12-05 09:36:16 +0100

    Add solution to part 1 of day 5

---
+++ 2022/day-5/part-1.js
@@ -0,0 +1,39 @@
+import { readFileSync } from "fs";
+import { join as pathJoin } from "path";
+
+const FILE = "input.txt";
+
+const data = readFileSync( pathJoin(__dirname, FILE) )
+	.toString()
+	.split("\n");
+// const crates = data.slice( 0, data.findIndex(line => !line) );
+const crates = {
+	1: [ "D", "M", "S", "Z", "R", "F", "W", "N" ],
+	2: [ "W", "P", "Q", "Q", "S", ],
+	3: [ "W", "R", "V", "Q", "F", "N", "J", "C" ],
+	4: [ "F", "Z", "P", "C", "G", "D", "L", ],
+	5: [ "T", "P", "S", ],
+	6: [ "H", "D", "F", "W", "R", "L", ],
+	7: [ "Z", "N", "D", "C", ],
+	8: [ "W", "N", "R", "F", "V", "S", "J", "Q" ],
+	9: [ "R", "M", "S", "G", "Z", "W", "V", ],
+};
+
+data.slice( data.findIndex(line => !line) + 1, data.length )
+	.filter( line => !!line )
+	.map( line =>
+		/move (\d+) from (\d+) to (\d+)/.exec(line)
+			.slice(1, 4)
+			.map( num => parseInt(num) )
+	)
+	.forEach( ([ count, from, to ]) =>
+		Array.from( Array(count) ).forEach( () =>
+			crates[to].push( crates[from].pop() )
+		)
+	);
+
+const topCrates = Object.values(crates)
+	.map( stack => stack[stack.length - 1] )
+	.join("");
+
+console.log(topCrates);
